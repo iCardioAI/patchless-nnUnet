1d511f74a29fecad212a4cf53644d6ff72737990
arnaudjudge <arnaud.judge@usherbrooke.ca>
2024-11-07 12:17:27 -0500

    add logger to logged hyperparams

--- patchless_nnunet/utils/logging_utils.py
+++ patchless_nnunet/utils/logging_utils.py
@@ -45,6 +45,7 @@ def log_hyperparameters(object_dict: dict) -> None:
     hparams["tags"] = cfg.get("tags")
     hparams["ckpt_path"] = cfg.get("ckpt_path")
     hparams["seed"] = cfg.get("seed")
+    hparams['logger'] = cfg.get("logger")
 
     # send hparams to all loggers
     for logger in trainer.loggers:
